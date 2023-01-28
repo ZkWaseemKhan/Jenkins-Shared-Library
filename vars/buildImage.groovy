0df30aa72a583491ec107f08dc7eb4befaa0f024
Waseem <waseem.khan4034@gmail.com>
2023-01-28 23:29:58 +0530

    getting with paramaters

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,11 +1,11 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(String imageName) {
     echo "Making docker Image and Pushing to docker started -- from shared repo"
     withCredentials([usernamePassword(credentialsId: 'docker-repo', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-       sh "docker build -t waseemkhandocker/my-docker-repo:java-maven-app-from-shared-3.0 ."
+       sh "docker build -t $imageName ."
        sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-       sh "docker push waseemkhandocker/my-docker-repo:java-maven-app-from-shared-3.0"
+       sh "docker push $imageName"
     }
     echo "Creating Image and Pushing to Docker has finished"
 }
