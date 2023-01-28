d0863af2774cb87a898ee0da36400469e14b6e81
Waseem <waseem.khan4034@gmail.com>
2023-01-28 23:10:28 +0530

    mistake verified 1

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -3,9 +3,9 @@
 def call() {
     echo "Making docker Image and Pushing to docker started -- from shared repo"
     withCredentials([usernamePassword(credentialsId: 'docker-repo', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-       sh "docker build -t waseemkhandocker/java-maven-app-from-idle:3.0 ."
+       sh "docker build -t waseemkhandocker/my-docker-repo:java-maven-app-from-shared-3.0 ."
        sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-       sh "docker push waseemkhandocker/java-maven-app-from-idle:3.0"
+       sh "docker push waseemkhandocker/my-docker-repo:java-maven-app-from-shared-3.0"
     }
     echo "Creating Image and Pushing to Docker has finished"
 }
