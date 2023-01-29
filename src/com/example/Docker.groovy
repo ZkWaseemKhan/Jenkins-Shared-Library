db13c03ebf702ff3965b86067ced027baa8ff624
Waseem <waseem.khan4034@gmail.com>
2023-01-29 11:22:33 +0530

    This is image docker making resolving error 3

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -2,11 +2,11 @@
 package com.example
 class Docker implements Serializable {
     def script
-    Docker(script){
+    Docker(script) {
         echo "Calling docker class from global 2"
         this.script = script
     }
-    def buildDockerImage(String imageName){
+    def buildDockerImage(String imageName) {
         script.echo "This is from class of Docker"
         script.echo "Making docker Image and Pushing to docker started -- from shared repo"
         script.withCredentials([script.usernamePassword(credentialsId: 'docker-repo', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
@@ -17,3 +17,14 @@ class Docker implements Serializable {
         script.echo "Creating Image and Pushing to Docker has finished"
     }
 }
+
+
+
+
+
+
+
+
+
+
+
