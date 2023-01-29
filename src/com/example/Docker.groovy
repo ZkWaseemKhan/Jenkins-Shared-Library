6c4bb55d84b707efb8de02caeb9d3edad7594a39
Waseem <waseem.khan4034@gmail.com>
2023-01-29 10:59:46 +0530

    Adding a project

---
+++ src/com/example/Docker.groovy
@@ -0,0 +1,17 @@
+#!/usr/bin/env groovy
+package com.example
+class Docker implements Serializable {
+    def script
+    Docker(script){
+        this.script = script
+    }
+    def buildDockerImage(String imageName){
+        script.echo "Making docker Image and Pushing to docker started -- from shared repo"
+        script.withCredentials([script.usernamePassword(credentialsId: 'docker-repo', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+            script.sh "docker build -t $imageName ."
+            script.sh "echo $script.PASSWORD | docker login -u $script.USERNAME --password-stdin"
+            script.sh "docker push $imageName"
+        }
+        script.echo "Creating Image and Pushing to Docker has finished"
+    }
+}
--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,12 +1,6 @@
 #!/usr/bin/env groovy
-
+package com.example.Docker
 def call(String imageName) {
-    echo "Making docker Image and Pushing to docker started -- from shared repo"
-    withCredentials([usernamePassword(credentialsId: 'docker-repo', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-       sh "docker build -t $imageName ."
-       sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-       sh "docker push $imageName"
-    }
-    echo "Creating Image and Pushing to Docker has finished"
-}
+    return new Docker(this).buildDockerImage(imageName)
 
+}
